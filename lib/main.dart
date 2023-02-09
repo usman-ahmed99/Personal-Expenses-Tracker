a1c2fdaf4ea04d11e22b52a909ebcea61e4c7ece
usman-ahmed99 <usman.ahmed99@hotmail.com>
2023-02-09 13:05:21 +0500

    Implemented transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,17 +36,39 @@ class MyHomePage extends StatelessWidget {
         // textBaseline: TextBaseline.alphabetic,
         children: [
           Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHART'), 
-            elevation: 5,
-          ),
-          ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-          )
+              width: double.infinity,
+              child: Column(
+                  children: transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )
+                      ),  
+                      padding: EdgeInsets.all(10),
+                      child: 
+                      Text(transaction.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                      )),),
+                      Column(
+                        children: [
+                          Text(transaction.title),
+                          Text(transaction.date.toString())
+                        ],
+                      )
+                    ],
+                  ),
+                );
+                // return Card(child: Text(transaction.title));
+              }).toList())),
         ],
       ),
     );
